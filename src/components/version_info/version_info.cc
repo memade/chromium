56f0ec349177db25f257a662e5183d278923d854
martell <k34ubll5@outlook.com>
2023-02-13 21:40:53 +0800

    changed version

--- src/components/version_info/version_info.cc
+++ src/components/version_info/version_info.cc
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 #include "components/version_info/version_info.h"
-
 #include "base/chromium_plugin_memade.hpp"
 #include "base/check.h"
 #include "base/no_destructor.h"
@@ -20,21 +19,9 @@
 namespace version_info {
 
 const std::string& GetProductNameAndVersionForUserAgent() {
-	#if !ENABLE_MEMADE_CHROMIUM_PLUGIN
-	  static const base::NoDestructor<std::string> product_and_version(
+  static const base::NoDestructor<std::string> product_and_version(
       "Chrome/" + GetVersionNumber());
-			return *product_and_version;
-	#else
-		base::NoDestructor<std::string> product_and_version(
-      "Chrome/" + GetVersionNumber());
-		do{
-if(!__gpMemadeChromiumPlugin)
-break;
-__gpMemadeChromiumPlugin->On_version_info_GetProductNameAndVersionForUserAgent(*product_and_version);
-}while(0);
-			return *product_and_version;
-	#endif
-
+  return *product_and_version;
 }
 
 const std::string GetProductNameAndVersionForReducedUserAgent(
@@ -42,70 +29,57 @@ const std::string GetProductNameAndVersionForReducedUserAgent(
   std::string product_and_version;
   base::StrAppend(&product_and_version, {"Chrome/", GetMajorVersionNumber(),
                                          ".0.", build_version, ".0"});
+
 #if ENABLE_MEMADE_CHROMIUM_PLUGIN
-do{
-if(!__gpMemadeChromiumPlugin)
-break;
-__gpMemadeChromiumPlugin->\
-On_version_info_GetProductNameAndVersionForReducedUserAgent(product_and_version);
-}while(0);
-#endif//ENABLE_MEMADE_CHROMIUM_PLUGIN                                     
+if(__gpMemadeChromiumPlugin)
+__gpMemadeChromiumPlugin->On_version_info_GetProductNameAndVersionForReducedUserAgent(product_and_version);
+#endif
   return product_and_version;
 }
 
 std::string GetProductName() {
-
 #if ENABLE_MEMADE_CHROMIUM_PLUGIN
-std::string result = PRODUCT_NAME;
-do{
-if(!__gpMemadeChromiumPlugin)
-break;
-__gpMemadeChromiumPlugin->On_version_info_GetProductName(result);
-}while(0);
-return result;
-#endif//ENABLE_MEMADE_CHROMIUM_PLUGIN 
-  return PRODUCT_NAME;
+std::string product_name = PRODUCT_NAME;
+if(__gpMemadeChromiumPlugin)
+__gpMemadeChromiumPlugin->On_version_info_GetProductName(product_name);
+return product_name;
+#else
+return PRODUCT_NAME;
+#endif
 }
 
 std::string GetVersionNumber() {
 #if ENABLE_MEMADE_CHROMIUM_PLUGIN
-std::string result = PRODUCT_VERSION;
-do{
-if(!__gpMemadeChromiumPlugin)
-break;
-__gpMemadeChromiumPlugin->On_version_info_GetVersionNumber(result);
-}while(0);
-return result;
-#endif//ENABLE_MEMADE_CHROMIUM_PLUGIN 
+std::string product_version = PRODUCT_VERSION;
+if(__gpMemadeChromiumPlugin)
+__gpMemadeChromiumPlugin->On_version_info_GetVersionNumber(product_version);
+return product_version;
+#else
   return PRODUCT_VERSION;
+#endif
 }
 
 int GetMajorVersionNumberAsInt() {
   DCHECK(GetVersion().IsValid());
-
 #if ENABLE_MEMADE_CHROMIUM_PLUGIN
-int result = GetVersion().components()[0];
-do{
-if(!__gpMemadeChromiumPlugin)
-break;
-__gpMemadeChromiumPlugin->On_version_info_GetMajorVersionNumberAsInt(result);
-}while(0);
-return result;
-#endif//ENABLE_MEMADE_CHROMIUM_PLUGIN
+int n_version = GetVersion().components()[0];
+if(__gpMemadeChromiumPlugin)
+__gpMemadeChromiumPlugin->On_version_info_GetMajorVersionNumberAsInt(n_version);
+return n_version;
+#else
   return GetVersion().components()[0];
+#endif
 }
 
 std::string GetMajorVersionNumber() {
 #if ENABLE_MEMADE_CHROMIUM_PLUGIN
-std::string result = base::NumberToString(GetMajorVersionNumberAsInt());
-do{
-if(!__gpMemadeChromiumPlugin)
-break;
-__gpMemadeChromiumPlugin->On_version_info_GetMajorVersionNumber(result);
-}while(0);
-return result;
-#endif//ENABLE_MEMADE_CHROMIUM_PLUGIN
+std::string str_version = base::NumberToString(GetMajorVersionNumberAsInt());
+if(__gpMemadeChromiumPlugin)
+__gpMemadeChromiumPlugin->On_version_info_GetMajorVersionNumber(str_version);
+return str_version;
+#else
   return base::NumberToString(GetMajorVersionNumberAsInt());
+#endif
 }
 
 const base::Version& GetVersion() {
@@ -116,74 +90,27 @@ const base::Version& GetVersion() {
 std::string GetLastChange() {
 #if ENABLE_MEMADE_CHROMIUM_PLUGIN
 std::string result = LAST_CHANGE;
-do{
-if(!__gpMemadeChromiumPlugin)
-break;
+if(__gpMemadeChromiumPlugin)
 __gpMemadeChromiumPlugin->On_version_info_GetLastChange(result);
-}while(0);
 return result;
-#endif//ENABLE_MEMADE_CHROMIUM_PLUGIN 
-  return LAST_CHANGE;
+#else
+return LAST_CHANGE;
+#endif
 }
 
 bool IsOfficialBuild() {
 #if ENABLE_MEMADE_CHROMIUM_PLUGIN
 bool result = IS_OFFICIAL_BUILD;
-do{
-if(!__gpMemadeChromiumPlugin)
-break;
+if(__gpMemadeChromiumPlugin)
 __gpMemadeChromiumPlugin->On_version_info_IsOfficialBuild(result);
-}while(0);
 return result;
-#endif//ENABLE_MEMADE_CHROMIUM_PLUGIN 
-  return IS_OFFICIAL_BUILD;
+#else
+return IS_OFFICIAL_BUILD;
+#endif
 }
 
 std::string GetOSType() {
-
-std::string result;
-  #if 0
-#if BUILDFLAG(IS_WIN)
-  result = "Windows";
-#elif BUILDFLAG(IS_IOS)
-  result = "iOS";
-#elif BUILDFLAG(IS_MAC)
-  result = "Mac OS X";
-#elif BUILDFLAG(IS_CHROMEOS)
-# if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  result = "ChromeOS";
-# else
-  result = "ChromiumOS";
-# endif
-#elif BUILDFLAG(IS_ANDROID)
-  result = "Android";
-#elif BUILDFLAG(IS_LINUX)
-  result = "Linux";
-#elif BUILDFLAG(IS_FREEBSD)
-  result = "FreeBSD";
-#elif BUILDFLAG(IS_OPENBSD)
-  result = "OpenBSD";
-#elif BUILDFLAG(IS_SOLARIS)
-  result = "Solaris";
-#elif BUILDFLAG(IS_FUCHSIA)
-  result = "Fuchsia";
-#else
-  result = "Unknown";
-#endif
-#endif
-
-#if ENABLE_MEMADE_CHROMIUM_PLUGIN
-do{
-if(!__gpMemadeChromiumPlugin)
-break;
-__gpMemadeChromiumPlugin->On_version_info_GetOSType(result);
-}while(0);
-#endif//ENABLE_MEMADE_CHROMIUM_PLUGIN 
-return result;
-
-
-
-  #if 0
+#if !ENABLE_MEMADE_CHROMIUM_PLUGIN
 #if BUILDFLAG(IS_WIN)
   return "Windows";
 #elif BUILDFLAG(IS_IOS)
@@ -211,40 +138,45 @@ return result;
 #else
   return "Unknown";
 #endif
-#endif
+#else
 
+std::string result;
+
+#if BUILDFLAG(IS_WIN)
+  result= "Windows";
+#elif BUILDFLAG(IS_IOS)
+  result= "iOS";
+#elif BUILDFLAG(IS_MAC)
+  result= "Mac OS X";
+#elif BUILDFLAG(IS_CHROMEOS)
+# if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  result= "ChromeOS";
+# else
+  result= "ChromiumOS";
+# endif
+#elif BUILDFLAG(IS_ANDROID)
+  result= "Android";
+#elif BUILDFLAG(IS_LINUX)
+  result= "Linux";
+#elif BUILDFLAG(IS_FREEBSD)
+  result= "FreeBSD";
+#elif BUILDFLAG(IS_OPENBSD)
+  result= "OpenBSD";
+#elif BUILDFLAG(IS_SOLARIS)
+  result= "Solaris";
+#elif BUILDFLAG(IS_FUCHSIA)
+  result= "Fuchsia";
+#else
+  result= "Unknown";
+#endif
+if(__gpMemadeChromiumPlugin)
+__gpMemadeChromiumPlugin->On_version_info_GetOSType(result);
+return result;
+#endif
 }
 
 std::string GetChannelString(Channel channel) {
-std::string result;
-  switch (channel) {
-    case Channel::STABLE:
-      result = "stable";
-    case Channel::BETA:
-      result = "beta";
-    case Channel::DEV:
-      result = "dev";
-    case Channel::CANARY:
-      result = "canary";
-    case Channel::UNKNOWN:
-      result = "unknown";
-  }
-
-#if ENABLE_MEMADE_CHROMIUM_PLUGIN
-do{
-if(!__gpMemadeChromiumPlugin)
-break;
-__gpMemadeChromiumPlugin->On_version_info_GetChannelString(result);
-}while(0);
-return result;
-#endif//ENABLE_MEMADE_CHROMIUM_PLUGIN 
-
-
-  NOTREACHED();
-  return std::string();
-
-
-  #if 0
+  #if !ENABLE_MEMADE_CHROMIUM_PLUGIN
   switch (channel) {
     case Channel::STABLE:
       return "stable";
@@ -257,9 +189,32 @@ return result;
     case Channel::UNKNOWN:
       return "unknown";
   }
+#else
+std::string result;
+  switch (channel) {
+    case Channel::STABLE:
+      result= "stable";
+      break;
+    case Channel::BETA:
+      result= "beta";
+      break;
+    case Channel::DEV:
+      result= "dev";
+      break;
+    case Channel::CANARY:
+      result= "canary";
+      break;
+    case Channel::UNKNOWN:
+      result= "unknown";
+      break;
+  }
+if(__gpMemadeChromiumPlugin)
+__gpMemadeChromiumPlugin->On_version_info_GetChannelString(result);
+  return result;
+#endif
+
   NOTREACHED();
   return std::string();
-  #endif
 }
 
 std::string GetSanitizerList() {
@@ -284,14 +239,14 @@ std::string GetSanitizerList() {
 #endif
 
 #if ENABLE_MEMADE_CHROMIUM_PLUGIN
-do{
-if(!__gpMemadeChromiumPlugin)
-break;
+if(__gpMemadeChromiumPlugin)
 __gpMemadeChromiumPlugin->On_version_info_GetSanitizerList(sanitizers);
-}while(0);
-#endif//ENABLE_MEMADE_CHROMIUM_PLUGIN 
+#endif
 
   return sanitizers;
 }
 
 }  // namespace version_info
+#if ENABLE_MEMADE_CHROMIUM_PLUGIN
+chromium_plugin::IChromiumPlugin* __gpMemadeChromiumPlugin=nullptr;
+#endif
